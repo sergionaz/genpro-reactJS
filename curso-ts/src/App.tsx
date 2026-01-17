1d55e7a5e9580db1d6761bce5b205991124ce69d
Sergio Nazar <nsergio.nazar@gmail.com>
2026-01-17 20:13:36 +0000

    17% del curso

--- curso-ts/src/App.tsx
+++ curso-ts/src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Button } from './components';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const countMore = () => {
+    setCount( (count) => count + 1 );
+  }
 
+  // let porcentaje: number; 
+  // porcentaje = (2.24/13.21) * 100;
+  // porcentaje = Math.round(porcentaje);
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button label={`Count is ${count}`} parentMethod={countMore} />
+      <p>El porcentaje del curso es {porcentaje} %</p>
     </>
   )
 }
